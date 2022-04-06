2560036b39e6df9a08ee9529f2e1d1e978f8fd41
namekosiru <rinjyou0031@toki.waseda.jp>
2022-04-06 09:24:59 +0900

    tsxfileの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,19 @@
-import type { NextPage } from "next";
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect } from "react";
-import { Box, chakra } from "@chakra-ui/react";
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { Box, chakra } from '@chakra-ui/react'
 
-import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
-import utilStyle from "../styles/utils.module.css";
-import { db } from "../Firebase/firebase";
+import Layout from '../components/Layout'
+import styles from '../styles/Home.module.css'
+import utilStyle from '../styles/utils.module.css'
+import { Profile } from '../components/Profile'
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    // Add a new document in collection "cities"
-    db.collection("users").add({
-      name: "namekosiru",
-      age: 100,
-      comment: "Big Fan",
-    });
-  }, []);
   return (
     <Layout>
-      <Box>aaa</Box>
-      <section className={utilStyle.headingMd}>
-        <p>私はエンジニア</p>
-      </section>
-      <section>
-        <h2>エンジニアブログ</h2>
-        <div className={styles.grid}>
-          <article>
-            <Link href="/">
-              <Image
-                src="/images/thumbnail01.jpg"
-                width={400}
-                height={400}
-                alt=""
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <br></br>
-            <Link href="/">
-              <a className={utilStyle.boldText}>使い分け.com</a>
-            </Link>
-            <br />
-            <small className={utilStyle.lightText}>2022 04 05</small>
-          </article>
-          <article>
-            <Link href="/">
-              <Image
-                src="/images/thumbnail01.jpg"
-                width={400}
-                height={400}
-                alt=""
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <br></br>
-            <Link href="/">
-              <a className={utilStyle.boldText}>使い分け.com</a>
-            </Link>
-            <br />
-            <small className={utilStyle.lightText}>2022 04 05</small>
-          </article>
-        </div>
-      </section>
+      <Profile></Profile>
+      <Box>aaa</Box>;
     </Layout>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- pages/sample/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from "next";
-
-export const MainPage: NextPage = () => {
-  return (
-    <div>
-      <p>test</p>
-    </div>
-  );
-};
