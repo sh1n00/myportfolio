e6b2ea85f3d2325e1b670c19ee31bf4467d74cd5
namekosiru <rinjyou0031@toki.waseda.jp>
2022-04-08 11:02:09 +0900

    レスポンシブの追加

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,3 +1,4 @@
+import useMedia from 'use-media'
 import { Box, Flex, Heading, Badge } from '@chakra-ui/react'
 
 import styles from './skill.module.css'
@@ -5,6 +6,52 @@ import styles from './skill.module.css'
 import { Lang, Tool, Certification } from './data/SkillData'
 
 export const Skill = () => {
+  const isWide = useMedia({ minWidth: '768px' })
+  return <>{isWide ? <PcWindow /> : <SpWindow />}</>
+}
+
+export const SpWindow = () => {
+  return (
+    <Box>
+      <Heading size="lg" textAlign="center" mb={2}>
+        Lang
+      </Heading>
+      <Box justifyContent="center">
+        {Lang.map((data, i) => (
+          <Badge key={i} className={styles.badge} m={1}>
+            {data}
+          </Badge>
+        ))}
+      </Box>
+
+      <Heading size="lg" textAlign="center" mb={2}>
+        Tool
+      </Heading>
+      <Box justifyContent="center">
+        {Tool.map((data, i) => (
+          <Badge key={i} className={styles.badge} m={1}>
+            {data}
+          </Badge>
+        ))}
+      </Box>
+
+      <Box>
+        <Heading size="lg" textAlign="center" mb={2}>
+          Certification
+        </Heading>
+        <Box justifyContent="center">
+          {Certification.map((data, i) => (
+            <Badge key={i} className={styles.badge} m={1}>
+              {data}
+            </Badge>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export const PcWindow = () => {
   return (
     <Flex>
       <Box display="block" justifyContent="center" width="50%" pl={10}>
